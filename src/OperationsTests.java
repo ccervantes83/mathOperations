df128b0a569b8410542ab3d16f8bd7393371738d
Carlos Mario Cervantes Ariza <ccervantes83@uan.edu.co>
2022-05-06 00:08:30 -0500

    Blue - oper - Refactor de la duplicidad de variables(Prueba exitosa)

--- src/OperationsTests.java
+++ src/OperationsTests.java
@@ -1,17 +1,23 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class operationsTests {
 
+	private Operation oper;
+
+	@Before
+	public void setUp() throws Exception{
+		oper = new Operation();
+	}
+	
 	@Test
-	public void OperationTest() {
-		Operation oper = new Operation();
+	public void OperationTest() throws Exception{
 		Assert.assertEquals(0,oper.value());
 	}
 	
 	@Test
-	public void OperationAdd() {
-		Operation oper = new Operation();
+	public void OperationAdd() throws Exception{
 		oper.add();
 	}
 
